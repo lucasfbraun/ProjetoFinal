fea7edbf3c016ae749ba3f0c8fd2d8d20cd7c4cb
luicas <lucasfbraun@gmail.com>
2017-08-15 19:53:06 -0300

    teste
    teste

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Random()
+        {
+            var	movie =	new	Movies(){nome = "Teste!"};
+            return View(movie);
+        }
+	}
+}
